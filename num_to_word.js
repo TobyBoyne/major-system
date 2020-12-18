bd10ea77120ab0f9eb2b67c48fa88647f1f82571
TobyBoyne <tobyboyne@gmail.com>
2020-12-18 13:23:05 +0000

    Added ability to highlight text; added npm and jQuery

--- num_to_word.js
+++ num_to_word.js
@@ -1,4 +1,15 @@
-function selectText() {
+/**
+ * Script for highlighting numbers, and suggesting words
+ */
+
+
+function selectText(textarea) {
     const num = window.getSelection().toString()
+    const start = textarea.selectionStart
+    const end = textarea.selectionEnd
+    console.log(start, end)
     console.log(num)
+    $('.numInput').highlightWithinTextarea({
+        highlight: [start, end]
+    })
 }
